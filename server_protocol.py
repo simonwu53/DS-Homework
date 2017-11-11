a27ce9f825c7d0c9fd1507f0790e797370a602f0
andro <xatiakilanavaa@yahoo.com>
2017-11-11 22:05:26 +0200

    changes

--- server_protocol.py
+++ server_protocol.py
@@ -7,6 +7,7 @@ import operator
 FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
+
 # Protocol Recv Size ---------------------------------------------------------
 #TCP_RECEIVE_BUFFER_SIZE=1024*1024
 #MAX_PDU_SIZE = 200*1024*1024
@@ -20,7 +21,7 @@ sudoku = [] #here will be stored sudoku and player limits
 user={} #this dictionary contains game_id as key which correspondes to the players with their scores
 id=0 # game id , it will be increased as new games are created one by one
 #sudoku_generator=[]
-sudoku_answer={}
+answers={}
 # Requests --------------------------------------------------------------------
 # Requests --------------------------------------------------------------------
 # client requests
@@ -108,11 +109,11 @@ def sudoku(gameid):
     message = __REQ_SUDOKU + MSG_SEP + ''.join(sudoku)    
     
     return message
-    
-def server_process(message,client_socket,server_socket):
+
+def server_process(message, client_socket, server_socket):
     '''Process the client's message,
         @param message: string, protocol data unit received from client
-                @returns string, response to send to client
+        @returns string, response to send to client
     '''
     LOG.debug('Received request [%d bytes] in total' % len(message))
     if len(message) < 2:
@@ -126,146 +127,72 @@ def server_process(message,client_socket,server_socket):
         else:
             user_names.append(msg)
             return __RSP_OK
-    if message.startswith(__REQ_JOIN + MSG_SEP): #here is 2 step a) user wants to join existing game or b)creating new game
-        msg=message[2:]
-        split_msg=msg.split(DATA_SEP)
-        var=split_msg[0]
+    if message.startswith(
+                    __REQ_JOIN + MSG_SEP):  # here is 2 step a) user wants to join existing game or b)creating new game
+        msg = message[2:]
+        split_msg = msg.split(DATA_SEP)
+        var = split_msg[0]
         if var.isdigit():
-             #if user wants to join existing game
-            name=split_msg[1]
-            game_id=int(var)
-            if(len(user[game_id])>game[game_id][1]):
-                return __RSP_JOINFAIL # limit is already reached at this game session
-            user[game_id][name] =0 #otherwise user is registered to the wanted game session and started from the score 0.
+            # if user wants to join existing game
+            name = split_msg[1]
+            game_id = int(var)
+            if (len(user[game_id]) > game[game_id][1]):
+                return __RSP_JOINFAIL  # limit is already reached at this game session
+            user[game_id][name] = [0,
+                                   client_socket]  # otherwise user is registered to the wanted game session and started from the score 0.
             return __RSP_OK
-        else:# else user wants to create new session
+        else:  # else user wants to create new session
             name = split_msg[0]
             limit = split_msg[1]
             difficulty = split_msg[2]
-            id+=1 # game ides will start from 1
-            str_id=str(id)
-            sudoku_answer, generated_sudoku=sudoku_generator.setup_sudoku(difficulty)
+            id += 1  # game ids will start from 1
+            str_id = str(id)
+            sudoku_answer, generated_sudoku = sudoku_generator.setup_sudoku(difficulty)
             sudoku.append(generated_sudoku)
             sudoku.append(limit)
-            game[id]=sudoku
-            answers[id]=sudoku_answer
-            score[name] = [0, client_socket]  # add user 1
-            User[id] = score
-            return MSG_SEP.join((__RSP_OK,)+tuple(str_id))
+            game[id] = sudoku
+            answers[id] = sudoku_answer
+            score[name] = [0, client_socket]  # saving score and client socket
+            user[id][name] = score
+            return MSG_SEP.join((__RSP_OK,) + tuple(str_id))
+
     if message.startswith(REQ_SUDOKU + MSG_SEP):
         msg = ''
         for key in game:
-            numb_players=str(len(user[key]))
-            msg+=str(key)+DATA_SEP+''.join(game[key][0])+DATA_SEP+game[key][1]+DATA_SEP+numb_players
-        msg += MSG_SEP
-        return __RSP_OK+ MSG_SEP+msg
-
-    if message.startswith(__REQ_USER + MSG_SEP):
-        msg = ''
-        for key in user:
-            msg += str(key)
-            for user_name in User[key]:  # will be returned in request_user
-                msg += DATA_SEP + user_name + DATA_SEP + str(User[key][user_name])
+            numb_players = str(len(user[key]))
+            msg += str(key) + DATA_SEP + ''.join(game[key][0]) + DATA_SEP + game[key][1] + DATA_SEP + numb_players
         msg += MSG_SEP
         return __RSP_OK + MSG_SEP + msg
+        # 1:1/1234/5/4:   this kind of string will be returned. header:gameid:sudoku:limit:players
+    if message.startswith(__REQ_USER + MSG_SEP):
+        msg1 = message[2:]
+        split_msg = msg1.split(DATA_SEP)
+        game_id = split_msg[0]
+        msg = ''
 
-    if message.startswith(__REQ_QUIT + MSG_SEP):
+        for user_name in user[game_id]:  # will be returned in request_user
+            msg += DATA_SEP + user_name + DATA_SEP + str(user[game_id][user_name][0])
+        msg += MSG_SEP
+        return __RSP_OK + MSG_SEP + msg
+        # 1:user4/0:User2/5:user3/8:user1/0:  this kind of string will be returned . header:username/score:username/score:
+
+    if message.startswith(__REQ_QUIT + MSG_SEP):  # user wants to quit. So his score will be deleted.
         msg = message[2:]
         split_msg = msg.split(DATA_SEP)
-        user_name=split_msg[1]
-        game_id=split_msg[0]
+        user_name = split_msg[1]
+        game_id = split_msg[0]
         del user[game_id][user_name]
-        del user[game_id][user_name][1]
+
         # msg =req_notify
         # thread=(targer='')
         if len(user[game_id]) == 0:
             del user[game_id]
             del game[game_id]
             del sudoku_answer[game_id]
-        if split_msg[2]=='close':
+        if split_msg[2] == 'close':
             return 'close'
 
-
         return __RSP_OK
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    elif message.startswith(__REQ_LAST + __MSG_FIELD_SEP):
-        s = message[2:]
-        try:
-            n = int(s)
-            LOG.debug('New message listing request from %s:%d, '\
-                      'messages %d' % (source+(n,)))
-        except ValueError:
-            LOG.debug('Integer required, %s received' % s)
-            return __RSP_BADFORMAT
-        ids = board.last(n)
-        LOG.debug('Last %d ids: %s ' % (n,','.join(map(str,ids))))
-        return __MSG_FIELD_SEP.join((__RSP_OK,)+tuple(map(str,ids)))
-    elif message.startswith(__REQ_GET + __MSG_FIELD_SEP):
-        s = message[2:]
-        try:
-            m_id = int(s)
-            LOG.debug('New message request by id from %s:%d, '\
-                      'id %d' % (source+(m_id,)))
-        except ValueError:
-            LOG.debug('Integer required, %s received' % s)
-            return __RSP_BADFORMAT
-        m = board.get(m_id)
-        if m == None:
-            LOG.debug('No messages by iD: %d' % m_id)
-            return __RSP_MSGNOTFOUND
-        m = map(str,m)
-        return __MSG_FIELD_SEP.join((__RSP_OK,)+tuple(m))
-    elif message.startswith(__REQ_GET_N_LAST + __MSG_FIELD_SEP):
-        s = message[2:]
-        try:
-            n = int(s)
-            LOG.debug('Client %s:%d requests %s last'\
-                      'messages' % (source+('all' if n <= 0 else '%d' % n,)))
-        except ValueError:
-            LOG.debug('Integer required, %s received' % s)
-            return __RSP_BADFORMAT
-        # Get last N ids
-        ids = board.last(n)
-        # Get messages by ids
-        msgs = map(board.get,ids)
-        # Turn everything to string, use space to separate message meta-info
-        #  [ "<timestamp> <ip> <port> <message>", ... ]
-        msgs = map(lambda x: ' '.join(map(str,x)),msgs)
-        return __MSG_FIELD_SEP.join((__RSP_OK,)+tuple(msgs))
-
-    #STEP TWO++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    elif message.startswith(__REQ_sendfile+__MSG_FIELD_SEP):
-        s=message[2:]
-
-        if os.path.exists(s):
-            #exists such file
-            print' such file already exists in the directory'
-            return __RSP_FILE_EXIST
-        else:
-            #not exists
-            sock.sendall(__RSP_OK)
-            rsp=tcp_receive(sock)
-            with open(s, 'w') as file_to_save:
-                file_to_save.write(rsp)
-            file_to_save.close()
-            print' file sucesfully uploaded '
-            return __RSP_OK
-
-
-
 
     else:
         LOG.debug('Unknown control message received: %s ' % message)
